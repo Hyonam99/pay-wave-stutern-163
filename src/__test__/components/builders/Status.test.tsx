222c059072c11528b547a3a285319c9c30b9e90e
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-12-10 11:50:13 +0100

    edit description for the status component test

--- src/__test__/components/builders/Status.test.tsx
+++ src/__test__/components/builders/Status.test.tsx
@@ -5,19 +5,19 @@ import { Status } from "components";
 
 describe("test the customer bar component", () => {
     
-    it("renders the status component with for paid invoices", () => {
+    it("renders the status component for paid invoices", () => {
         const { getByTestId } = render(<Status status="paid"/>)
         expect(getByTestId("paid-status-component")).toBeInTheDocument()
         expect(getByTestId("paid-status-component")).toHaveTextContent("Paid")
     })
 
-    it("renders the status component with for pending invoices", () => {
+    it("renders the status component for pending invoices", () => {
         const { getByTestId } = render(<Status status="pending"/>)
         expect(getByTestId("pending-status-component")).toBeInTheDocument()
         expect(getByTestId("pending-status-component")).toHaveTextContent("Pending")
     })
 
-    it("renders the status component with for draft invoices", () => {
+    it("renders the status component for draft invoices", () => {
         const { getByTestId } = render(<Status status="draft"/>)
         expect(getByTestId("draft-status-component")).toBeInTheDocument()
         expect(getByTestId("draft-status-component")).toHaveTextContent("Draft")
